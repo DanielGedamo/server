06b558cbf887e0e0345e096d3f4be0c38a1ce49a
DANIELGEDAMO <danielgadamo@gmail.com>
2022-12-26 17:39:31 +0200

    add

--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,19 @@
-import User from "../models/User.js";
+import User from "../models/User.js"
 
 /* READ */
-export const getUser = async (req, res) => {
+export const getUser = async (request, response) => {
   try {
-    const { id } = req.params;
+    const { id } = request.params;
     const user = await User.findById(id);
-    res.status(200).json(user);
+    response.status(200).json(user);
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    response.status(404).json({ message: err.message });
   }
 };
 
-export const getUserFriends = async (req, res) => {
+export const getUserFriends = async (request, response) => {
   try {
-    const { id } = req.params;
+    const { id } = request.params;
     const user = await User.findById(id);
 
     const friends = await Promise.all(
@@ -24,16 +24,16 @@ export const getUserFriends = async (req, res) => {
         return { _id, firstName, lastName, occupation, location, picturePath };
       }
     );
-    res.status(200).json(formattedFriends);
+    response.status(200).json(formattedFriends);
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    response.status(404).json({ message: err.message });
   }
 };
 
 /* UPDATE */
-export const addRemoveFriend = async (req, res) => {
+export const addRemoveFriend = async (request, response) => {
   try {
-    const { id, friendId } = req.params;
+    const { id, friendId } = request.params;
     const user = await User.findById(id);
     const friend = await User.findById(friendId);
 
@@ -56,8 +56,8 @@ export const addRemoveFriend = async (req, res) => {
       }
     );
 
-    res.status(200).json(formattedFriends);
+    response.status(200).json(formattedFriends);
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    response.status(404).json({ message: err.message });
   }
 };
